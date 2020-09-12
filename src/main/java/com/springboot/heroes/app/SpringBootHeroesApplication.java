2d7ed6a09dae6c5484abe56fdc177bb9829e324a
franrbb <fran89rbb@hotmail.com>
2020-09-12 14:36:56 +0200

    Creacion de proyecto Heoes con Spring Boot

---
+++ src/main/java/com/springboot/heroes/app/SpringBootHeroesApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.heroes.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHeroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHeroesApplication.class, args);
+	}
+
+}
