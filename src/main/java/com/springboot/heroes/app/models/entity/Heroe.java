18cdc8103537dfbba9599be5990a20358e6484a1
franrbb <fran89rbb@hotmail.com>
2020-09-14 16:32:44 +0200

    Creacion de la clase Heroe con sus atibutos

---
+++ src/main/java/com/springboot/heroes/app/models/entity/Heroe.java
@@ -0,0 +1,72 @@
+package com.springboot.heroes.app.models.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="heroes")
+public class Heroe {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	private String descripcion;
+	private String casa;
+	private Date aparicion;
+	private String imagen;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getCasa() {
+		return casa;
+	}
+
+	public void setCasa(String casa) {
+		this.casa = casa;
+	}
+
+	public Date getAparicion() {
+		return aparicion;
+	}
+
+	public void setAparicion(Date aparicion) {
+		this.aparicion = aparicion;
+	}
+
+	public String getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+}
